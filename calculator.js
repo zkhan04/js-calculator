e715bd43efa10254986e60ed04c35428a16c18da
zkhan04 <khanzayd11@gmail.com>
2024-01-09 22:45:39 -0600

    got the decimal button to work
    will get unary operators working next commit

--- calculator.js
+++ calculator.js
@@ -19,4 +19,25 @@ clearButton.addEventListener('click', clearCalculator);
 // once again, let's move this functionality into another function in case we change it around
 function clearCalculator(){
     inputBox.textContent = '';
+    enableDecimal();
+}
+
+// let's add decimals into our thingimabobber now!
+let decimalButton = document.querySelector('#dot');
+decimalButton.addEventListener('click', disableDecimal)
+
+// this is good, but pressing it should disable the button. let's move this as well!
+function disableDecimal(){
+    inputBox.textContent += '.';
+    decimalButton.disabled = true;
+    decimalButton.setAttribute('style', 'background-color: gray');
+}
+
+// doing great! now we just need to re-enable the button when we press AC DONE :)
+// however, there are multiple occasions where we need to enable the decimal button without clearing the calculator, so we'll make
+// a separate function
+
+function enableDecimal(){
+    decimalButton.disabled = false;
+    decimalButton.setAttribute('style', 'background-color: buttonface');
 }
