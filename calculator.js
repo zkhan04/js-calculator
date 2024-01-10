1b2a3b02f9a26e5a32d8c8a6053fed02a394ab25
zkhan04 <khanzayd11@gmail.com>
2024-01-09 23:01:22 -0600

    unary operators working. now let's handle binary :)

--- calculator.js
+++ calculator.js
@@ -7,36 +7,50 @@ digits.forEach(digit => {
     digit.addEventListener('click', (e) => fillInputBox(e));
 });
 
+// clicking AC should clear the input box.
+let clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', clearCalculator);
+
+// let's add decimals into our thingimabobber now!
+let decimalButton = document.querySelector('#dot');
+decimalButton.addEventListener('click', disableDecimal);
+
+// now let's get the unary operators to work.
+// first will be the percent
+let percentButton = document.querySelector('#percent');
+percentButton.addEventListener('click', () => {
+    let temp = parseFloat(inputBox.textContent);
+    temp /= 100;
+    inputBox.textContent = temp.toString();
+})
+
+// now let's do the sign toggle
+let signButton = document.querySelector('#sign-toggle');
+signButton.addEventListener('click', () => {
+    let temp = parseFloat(inputBox.textContent);
+    temp *= -1;
+    inputBox.textContent = temp.toString();
+})
+
 // let's move this functionality into another function in case we change things around!
 function fillInputBox(e) {
     inputBox.textContent += e.target.id;
 }
 
-// clicking AC should clear the input box.
-let clearButton = document.querySelector('#clear');
-clearButton.addEventListener('click', clearCalculator);
-
-// once again, let's move this functionality into another function in case we change it around
+// clears the calculator
 function clearCalculator(){
     inputBox.textContent = '';
     enableDecimal();
 }
 
-// let's add decimals into our thingimabobber now!
-let decimalButton = document.querySelector('#dot');
-decimalButton.addEventListener('click', disableDecimal)
-
-// this is good, but pressing it should disable the button. let's move this as well!
+// turn off the decimal
 function disableDecimal(){
     inputBox.textContent += '.';
     decimalButton.disabled = true;
     decimalButton.setAttribute('style', 'background-color: gray');
 }
 
-// doing great! now we just need to re-enable the button when we press AC DONE :)
-// however, there are multiple occasions where we need to enable the decimal button without clearing the calculator, so we'll make
-// a separate function
-
+// turn the decimal back on!
 function enableDecimal(){
     decimalButton.disabled = false;
     decimalButton.setAttribute('style', 'background-color: buttonface');
