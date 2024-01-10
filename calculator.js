71331961a9654087aaab5bcba68e18d79926736c
zkhan04 <khanzayd11@gmail.com>
2024-01-10 12:26:02 -0600

    binary operators work now :)
    will work on UI and rounding next commit

--- calculator.js
+++ calculator.js
@@ -34,6 +34,10 @@ signButton.addEventListener('click', () => {
 
 // let's move this functionality into another function in case we change things around!
 function fillInputBox(e) {
+    if (awaitingInput) {
+        inputBox.textContent = '';
+        awaitingInput = false;
+    }
     inputBox.textContent += e.target.id;
 }
 
@@ -41,10 +45,16 @@ function fillInputBox(e) {
 function clearCalculator(){
     inputBox.textContent = '';
     enableDecimal();
+    prev = null;
+    current = null;
 }
 
 // turn off the decimal
 function disableDecimal(){
+    if (awaitingInput) {
+        inputBox.textContent = '';
+        awaitingInput = false;
+    }
     inputBox.textContent += '.';
     decimalButton.disabled = true;
     decimalButton.setAttribute('style', 'background-color: gray');
@@ -55,3 +65,78 @@ function enableDecimal(){
     decimalButton.disabled = false;
     decimalButton.setAttribute('style', 'background-color: buttonface');
 }
+
+// time for binary operators!
+// when we call a binary operator, we will store the value in current.
+// once a value 'replaces' current, it should be placed into prev;
+
+let prev = null;
+let current = null;
+
+// having arrow functions for the four operators will help.
+
+let add = (n1, n2) => n1 + n2;
+let subtract = (n1, n2) => n1 - n2;
+let multiply = (n1, n2) => n1 * n2;
+let divide = (n1, n2) => n1 / n2;
+
+// okay, let's make buttons for each of the operators.
+let operators = document.querySelectorAll('.operator');
+operators.forEach(operator => {
+    operator.addEventListener('click', (e) => operator_press(e));
+})
+
+// what should happen when an operator is pressed?
+// if current is null, read the inputBox into current.
+// if current is not null, read current into prev then read inputBox into current.
+// at this point, combine prev and current into one and store the result into current.
+// then, the operator to run should be set to whatever was pressed.
+let operatorMatches = {
+    'add': add,
+    'subtract': subtract,
+    'multiply': multiply,
+    'divide': divide,
+};
+
+let operator;
+function operator_press(e){
+    if (current != null) {
+        prev = current;
+        current = parseFloat(inputBox.textContent);
+        current = operator(prev, current);
+        inputBox.textContent = current.toString()
+    } else {
+        current = parseFloat(inputBox.textContent);
+    }
+    awaitingInput = true;
+    enableDecimal();
+    let id = e.target.id;
+    operator = operatorMatches[id];
+}
+
+// after an operator is pressed, the next thing to be entered after that should 'clear' the input
+// after an operator is pressed, the decimal button should also be freed up
+let awaitingInput = true;
+
+// what should the equals thing do?
+// it should:
+// do the normal read into current + prev thing
+// if 
+
+function equal_press(){
+    if(current != null){
+        prev = current;
+        current = parseFloat(inputBox.textContent);
+        current = operator(prev, current);
+        inputBox.textContent = current.toString();
+        current = null;
+    } else {
+        current = parseFloat(inputBox.textContent);
+    }
+    awaitingInput = true;
+    enableDecimal();
+}
+
+let equalButton = document.querySelector('#equals');
+equalButton.addEventListener('click', equal_press);
+
