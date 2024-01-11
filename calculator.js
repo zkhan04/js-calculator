c57f9fb755fd66e77188653361aba037ea63f048
zkhan04 <khanzayd11@gmail.com>
2024-01-10 19:31:42 -0600

    JAVASCRIPT COMPLETE LETS GOOO
    - implemented rounding to 8 decimal places
    - made a few smaller changes which simplify code
    - it's at 100 lines now :)

--- calculator.js
+++ calculator.js
@@ -1,7 +1,6 @@
-// STATE VARIABLES
+//----------STATE VARIABLES----------
 // two variables used to store calculator memory
-let prev = null;
-let current = null;
+let prev = current = null;
 
 // the operator that the calculator should call (+, -, *, /)
 let operator;
@@ -9,106 +8,93 @@ let operator;
 // if the calculator should keep writing on top of whatever's displayed, or clear and start again.
 let awaitingInput = true;
 
-// where the text is being displayed.
+//----------SELECTING OBJECTS FROM DOM----------
+// calculator components
 let inputBox = document.querySelector('#input-box');
-
-// now let's allow the user to type into it
 let digits = document.querySelectorAll('.number');
+let binaryOperators = document.querySelectorAll('.binary-operator');
+let unaryOperators = document.querySelectorAll('.unary-operator');
+let clearButton = document.querySelector('#clear');
+let decimalButton = document.querySelector('#dot');
+
+// adding event listeners
 digits.forEach(digit => {
     digit.addEventListener('click', (e) => fillInputBox(e));
 });
+binaryOperators.forEach(operator => {
+    operator.addEventListener('click', (e) => binaryOperatorPress(e));
+})
+unaryOperators.forEach(operator => {
+    operator.addEventListener('click', (e) => unaryOperatorPress(e));
+})
+clearButton.addEventListener('click', clearCalculator);
+decimalButton.addEventListener('click', (e) => fillInputBox(e));
 
-// fills the input box whenever one of the numbers is pressed
+//----------CALLBACK FUNCTIONS----------
+// fills the input box whenever one of the numbers, or the dot is is pressed
 function fillInputBox(e) {
     if (awaitingInput) {
         inputBox.textContent = '';
         awaitingInput = false;
     }
-    inputBox.textContent += e.target.id;
+    if (e.target.id === 'dot') {
+        inputBox.textContent += '.';
+        decimalButton.disabled = true;
+        decimalButton.setAttribute('style', 'background-color: gray')
+    } else inputBox.textContent += e.target.id;
 }
 
-// clicking AC should clear the input box.
-let clearButton = document.querySelector('#clear');
-clearButton.addEventListener('click', clearCalculator);
+// callback when a binary operator is pressed
+function binaryOperatorPress(e){
+    readInputBox();
+    if (e.target.id === 'equals') current = null;
+    else operator = operatorMatches[e.target.id];
+}
 
-// clears the calculator
+// callback for when a unary operator is pressed
+function unaryOperatorPress(e){
+    let temp = parseFloat(inputBox.textContent);
+    let func = operatorMatches[e.target.id];
+    temp = func(temp);
+    inputBox.textContent = temp.toString();
+}
+
+// callback for AC, clears the calculator
 function clearCalculator(){
     inputBox.textContent = '';
-    enableDecimal();
     prev = null;
     current = null;
+    enableDecimal();
 }
 
-// let's add decimals into our thingimabobber now!
-let decimalButton = document.querySelector('#dot');
-decimalButton.addEventListener('click', disableDecimal);
-
-// turn off the decimal
-function disableDecimal(){
-    if (awaitingInput) {
-        inputBox.textContent = '';
-        awaitingInput = false;
-    }
-    inputBox.textContent += '.';
-    decimalButton.disabled = true;
-    decimalButton.setAttribute('style', 'background-color: gray');
-}
-
-// turn the decimal back on!
+//----------HELPER FUNCTIONS----------
+// turns the decimal back on - whenever a binary operator is pressed, or calculator is cleared
 function enableDecimal(){
     decimalButton.disabled = false;
     decimalButton.setAttribute('style', 'background-color: buttonface');
 }
 
-// buttons for each of the operators. when one is pressed, it calls the operator_press button
-let binaryOperators = document.querySelectorAll('.binary-operator');
-binaryOperators.forEach(operator => {
-    operator.addEventListener('click', (e) => binaryOperatorPress(e));
-})
-
-// an object which matches each button ID to one of the arrow functions will be helpful as well!
-let binaryOperatorMatches = {
+// an object which matches each operator ID to an arrow function
+let operatorMatches = {
     'add': ((n1, n2) => n1 + n2),
     'subtract': ((n1, n2) => n1 - n2),
     'multiply': ((n1, n2) => n1 * n2),
     'divide': ((n1, n2) => n1 / n2),
+    'percent': ((n) => n / 100),
+    'sign-toggle': ((n) => n * -1),
 };
 
-// reads from the input box.
+// reads from the input box into current. Evaluates an expression if necessary, then rounds
 function readInputBox(){
     if (current != null) {
         prev = current;
         current = parseFloat(inputBox.textContent);
         current = operator(prev, current);
+        current = Math.round(current * 1e8)/1e8
         inputBox.textContent = current.toString()
     } else {
         current = parseFloat(inputBox.textContent);
     }
     awaitingInput = true;
     enableDecimal();
-}
-
-// what happens when an operator is pressed
-function binaryOperatorPress(e){
-    readInputBox();
-    if (e.target.id === 'equals') current = null;
-    else operator = binaryOperatorMatches[e.target.id];
-}
-
-// let's apply a similar approach to unary operators!
-let unaryOperators = document.querySelectorAll('.unary-operator');
-unaryOperators.forEach(operator => {
-    operator.addEventListener('click', (e) => unaryOperatorPress(e));
-})
-
-function unaryOperatorPress(e){
-    let temp = parseFloat(inputBox.textContent);
-    let func = unaryOperatorMatches[e.target.id];
-    temp = func(temp);
-    inputBox.textContent = temp.toString();
-}
-
-let unaryOperatorMatches = {
-    'percent': ((n) => n / 100),
-    'sign-toggle': ((n) => n * -1),
 }
