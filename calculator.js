bace717453791fecf5aef7a7c679d31269b71182
zkhan04 <khanzayd11@gmail.com>
2024-01-16 13:57:46 -0600

    Binary operators now highlighted when you use them
    Added keyboard support for digits and the decimal

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+// CONSTANTS
+let enabledStyle = 'background-color: black';
+let disabledStyle = 'background-color: gray';
+
 //----------STATE VARIABLES----------
 // prev & current used to store calculator memory, operator used to store current operation
 let prev = current = currentOperator = null;
@@ -18,8 +22,8 @@ let decimalButton = document.querySelector('[id = \'.\']');
 digits.forEach(digit => {
     digit.addEventListener('click', (e) => fillInputBox(e.target.id));
 });
-binaryOperators.forEach(currentOperator => {
-    currentOperator.addEventListener('click', (e) => binaryOperatorPress(e));
+binaryOperators.forEach(operator => {
+    operator.addEventListener('click', (e) => binaryOperatorPress(e));
 })
 unaryOperators.forEach(operator => {
     operator.addEventListener('click', (e) => unaryOperatorPress(e));
@@ -28,19 +32,19 @@ clearButton.addEventListener('click', clearCalculator);
 decimalButton.addEventListener('click', (e) => fillInputBox(e.target.id));
 
 //----------CALLBACK FUNCTIONS----------
-// fills the input box whenever one of the numbers, or the dot is is pressed
+// fills the input box whenever one of the numbers, or the dot is pressed
 function fillInputBox(char) {
-    if (awaitingInput) {
-        inputBox.textContent = '';
-        awaitingInput = false;
-    }
+    if (awaitingInput) {inputBox.textContent = '';}
     if (char === '.') {disableDecimal();}
     inputBox.textContent += char;
+    awaitingInput = false;
 }
 
 // callback when a binary operator is pressed
 function binaryOperatorPress(e){
     readInputBox();
+    refreshButtons();
+    e.target.setAttribute('style', disabledStyle);
     if (e.target.id === 'equals') current = null;
     else operator = operatorMatches[e.target.id];
 }
@@ -57,22 +61,17 @@ function unaryOperatorPress(e){
 
 // callback for AC, clears the calculator
 function clearCalculator(){
+    refreshButtons()
     inputBox.textContent = '';
     prev = current = null;
-    enableDecimal();
+    
 }
 
 //----------HELPER FUNCTIONS----------
-// turns the decimal back on - whenever a binary operator is pressed, or calculator is cleared
-function enableDecimal(){
-    decimalButton.disabled = false;
-    decimalButton.setAttribute('style', 'background-color: black');
-}
-
 // turns decimal back off
 function disableDecimal(){
     decimalButton.disabled = true;
-    decimalButton.setAttribute('style', 'background-color: gray')
+    decimalButton.setAttribute('style', disabledStyle);
 }
 
 // an object which matches each operator ID to an arrow function
@@ -95,5 +94,35 @@ function readInputBox(){
         inputBox.textContent = current.toString()
     } else {current = parseFloat(inputBox.textContent);}
     awaitingInput = true;
-    enableDecimal();
 }
+
+// enables all of the buttons
+function refreshButtons(){
+    binaryOperators.forEach(operator => {
+        operator.setAttribute('style', enabledStyle);
+    })
+    decimalButton.disabled = false;
+    decimalButton.setAttribute('style', enabledStyle);
+}
+
+let body = document.querySelector('body')
+body.addEventListener('keyup', (e) => (readKeyboardUp(e.key)));
+body.addEventListener('keydown', (e) => readKeyboardDown(e.key))
+
+// check if shift is being pressed or not!
+let shiftPressed = false;
+
+function readKeyboardUp(key){
+    if (key === 'SHIFT') shiftPressed = false;
+    if (!shiftPressed) {
+        if (/[0-9.]/.test(key)) fillInputBox(key);
+    }
+}
+
+function readKeyboardDown(key){
+    if (key === 'SHIFT') shiftPressed = true;
+}
+
+// we should also highlight the operator that is currently selected, would be nice DONE
+
+
